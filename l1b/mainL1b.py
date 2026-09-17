a03020b832b56c711cf942d5772fa2fd05dc0141
Alessandro2307 <100613527@alumnos.uc3m.es>
2026-09-17 20:38:01 +0200

    lezione 2
    lezione 2

--- l1b/mainL1b.py
+++ l1b/mainL1b.py
@@ -4,9 +4,9 @@
 from l1b.src.l1b import l1b
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = r'C:\EODP\eodp_students\auxiliary'
-indir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\input"
-outdir = r"C:\EODP\EODP_TER_2021_working\EODP-TS-L1B\myoutputs"
+auxdir = r'C:\\Users\\Aless\\Desktop\\EARTH\\repo\\auxiliary'
+indir = r"C:C:\\Users\\SHARED\\EODP_TER_2021\\EODP-TS-ISM\\input\\gradient_alt100_act150"
+outdir = r"C:\\Users\\SHARED\\EODP_TER_2021\\EODP-TS-ISM\\myoutput_eq"
 
 # Initialise the ISM
 myL1b = l1b(auxdir, indir, outdir)
